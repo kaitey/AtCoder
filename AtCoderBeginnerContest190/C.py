e792ef4aefbed88c8dedb0d1f0b88a315f463401
KAWAHARA Takuya <re0104er@ed.ritsumei.ac.jp>
2021-04-07 15:00:22 +0900

    ABC190 C 回答

--- AtCoderBeginnerContest190/C.py
+++ AtCoderBeginnerContest190/C.py
@@ -1,11 +1,20 @@
 def main():
-    n, s, d = list(map(int, input().split()))
-    for i in range(n):
-        x, y = list(map(int, input().split()))
-        if x < s and y > d:
-            print("Yes")
-            return
-    print("No")
+    n, m = list(map(int, input().split()))
+    ab_s = [list(map(int, input().split())) for _ in range(m)]
+    k = int(input())
+    cds = [list(map(int, input().split())) for _ in range(k)]
+    ans = 0
+    for i in range(1 << k):
+        dish = [0] * n
+        num = "0" * k + bin(i)[2:]
+        cnt = 0
+        for j, cd in enumerate(reversed(cds)):
+            dish[cd[int(num[- j - 1])] - 1] += 1
+        for ab in ab_s:
+            if dish[ab[0] - 1] >= 1 and dish[ab[1] - 1] >= 1:
+                cnt += 1
+        ans = max(ans, cnt)
+    print(ans)
 
 
 if __name__ == '__main__':
